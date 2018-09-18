91f5bcdaa5aeb7327213b5b773c12956c961ef02
plvd.name <1690451238@qq.com>
2018-09-18 13:03:16 +0800

    歌手页面接口的提交，已过检

--- routes/singer.js
+++ routes/singer.js
@@ -1,6 +1,5 @@
 const router = require('koa-router')()
-const singerDAO=require('../model/singerDAO')
-const singerController=require('../document/singerController')
+const singerController=require('../controllers/SingerController')
 router.prefix('/singer')
 //根路由  所有歌手
 router.get('/', async (ctx, next)=> {
@@ -9,17 +8,10 @@ router.get('/', async (ctx, next)=> {
 //二级页面 歌手的歌曲
 router.get('/songlist/:singerid', async (ctx, next)=> {
     await singerController.getsingersong(ctx)
-    // let jsondata =await singerDAO.getsingersong(ctx.params.singerid);
-    // console.log(jsondata)
-    // ctx.set('content-type','application/json')
-    // ctx.body = {"code":200,"message":"ok",data:jsondata[0]};
 })
 //三级页面 歌曲播放
 router.get('/song/:songid',async (ctx,next)=>{
     await singerController.getonesong(ctx)
-    // let jsondata =await singerDAO.getonesong(ctx.params.songid);
-    // ctx.set('content-type','application/json')
-    // ctx.body = {"code":200,"message":"ok",data:jsondata[0]};
 })
 
 module.exports = router
