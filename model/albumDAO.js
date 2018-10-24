484dc5abeb2b8e566c09c97d5297daa95fc9050f
mxy <3041099338@qq.com>
2018-10-24 19:25:40 +0800

    评论表

--- model/albumDAO.js
+++ model/albumDAO.js
@@ -4,6 +4,10 @@ class DB {
     getallalbum(){
         return DAO('select * from album',[])
     }
+    //通过ID获取专辑信息
+    getalbum(id){
+        return DAO('select * from album where albumid=?',[id])
+    }
     //获取指定专辑内的所有歌曲
     getalbumsong(id){
         return DAO('select * from song where albumid=?',[id])
--- routes/album.js
+++ routes/album.js
@@ -6,6 +6,10 @@ router.prefix('/album')
 router.get('/', async (ctx, next)=> {
     await albumDontroller.getallalbum(ctx)
 })
+//
+router.get('/album1/:albumid', async (ctx, next)=> {
+    await albumDontroller.getalbum(ctx)
+})
 //二级页面 指定专辑内所有歌曲
 router.get('/details/:albumid', async (ctx, next)=> {
     await albumDontroller.getalbumsong(ctx)
