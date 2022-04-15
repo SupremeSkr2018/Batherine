e821b1b5c59bcbe49947a48d9e5f98de3e0fc2c7
Ray <43198142+SupremeSkr2018@users.noreply.github.com>
2022-04-15 11:02:23 +0800

    Delete controllers directory

--- controllers/workController.js
+++
@@ -1,8 +0,0 @@
-const workDAO=require('../model/workDAO')
-module.exports={
-    delwork:async(ctx,next)=>{
-        let jsondata =await workDAO.
-        ctx.set('content-type','application/json')
-        ctx.body={"code":200,"message":"ok",data:jsondata}
-    }
-}
